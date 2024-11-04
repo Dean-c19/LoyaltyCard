f81dbadd4c1855e601cf9d8e728f808169817bee
Dean-c19 <148439940+Dean-c19@users.noreply.github.com>
2024-11-04 21:07:48 +0000

    Commit 4
    Implemented another login function and the the modify points functionality.
    
    Checks so the user can go below 0 points.
    
    Tested all my functionalities and made sure database was updated.

--- src/ModifyPoints.java
+++ src/ModifyPoints.java
@@ -1,8 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,13 +24,58 @@ public class ModifyPoints extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        out.println("<h3>Hi, Youve modified your points</h3>");
+
 
         // log the user in, query by username to get ther username and password
-        // if the user exists, update the points in the db and display the new number
-        // otherwise display an error
-        // if theres not enough points to apply a negative number, display an error
+        String username = request.getParameter("username");
+        String passwordEntered = request.getParameter("password");
+        try {
+            String loginQuery = "SELECT * FROM members WHERE username = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(loginQuery);
+            preparedStatement.setString(1, username);
+
+            ResultSet output = preparedStatement.executeQuery();
+
+            if (output.next()) {
+
+                String passwordsStored = output.getString("password");
+                int newPoints = Integer.parseInt(request.getParameter("points"));
+                int points = output.getInt("points");
+                int modifiedPoints = points + newPoints;
+                // if the user exists and passwords match, update the points in the db and display the new number
+                if (passwordsStored.equals(passwordEntered) && modifiedPoints >= 0) {
+
+                    String updatePointsSql = "UPDATE members SET points = ? WHERE username = ?";
+                    PreparedStatement updateStatement = connection.prepareStatement(updatePointsSql);
+                    updateStatement.setInt(1, modifiedPoints);
+                    updateStatement.setString(2, username);
+                    updateStatement.executeUpdate();
 
 
+
+                    out.println("<h3>Points Updated!</h3>");
+                    out.println("<h3>New points balance for " + username + ": " + modifiedPoints + "</h3>");
+
+                }
+                // if theres not enough points to apply a negative number, display an error
+                else if (modifiedPoints < 0) {
+                    out.println("<h3>You dont have enough points to spend. Please try again.</h3>");
+                }
+                else {
+                    // if password not correct display error
+                    out.println("<h3>Incorrect password. Please try again.</h3>");
+                }
+            } else {
+                // username not found display error
+                out.println("<h3>Username doesnt exist. Please try again.</h3>");
+            }
+
+
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
